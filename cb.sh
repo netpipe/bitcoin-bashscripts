b1e9006cfbaa8bb43c925e26cfa94ea011660baa
netpipe <teslaenergy@gmail.com>
2022-11-20 22:00:07 -0700

    testing

--- cb.sh
+++ cb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #useful for waiting for payments or seeing your wallet balance live
 ADDRESS=bc1qu3ds562w2av52rz8xl0pm5jkk66s5ep95e4wwp
-if [$1 != ""] ; then ADDRESS=$1 ; fi # use alternate address if specified
+if [[ $1 != "" ]] ; then ADDRESS=$1 ; fi # use alternate address if specified
 
 while true;do
-#echo "test";
-wget -qO- https://blockchain.info/balance?active=$ADDRESS 2>&1 | grep -Po '"total_received":\K[0-9]+' | awk '{s=$1/100000000} END {printf "Received : %0.8f\n", s}'
+echo $ADDRESS;
+#wget -qO- https://blockchain.info/balance?active=$ADDRESS 2>&1 | grep -Po '"total_received":\K[0-9]+' | awk '{s=$1/100000000} END {printf "Received : %0.8f\n", s}'
 sleep 1500 #10-15 min
 done
 #0.05185304
