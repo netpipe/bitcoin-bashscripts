3d87ea9a2d9e2ab9d213a6d0f38fbc1d22d2313d
netpipe <teslaenergy@gmail.com>
2022-11-22 17:11:50 -0700

    fixes

--- cb.sh
+++ cb.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 #useful for waiting for payments or seeing your wallet balance live
 ADDRESS=bc1qu3ds562w2av52rz8xl0pm5jkk66s5ep95e4wwp
-
 oldbalance=0
+
 if [[ $1 != "" ]] ; then ADDRESS=$1 ; fi # use alternate address if specified
+
 while true;do
-echo $ADDRESS;
-wget -qO- https://blockchain.info/balance?active=$ADDRESS > ./tbalance
-#balance=$(wget -qO- https://blockchain.info/balance?active=$ADDRESS 2>&1 | grep -Po '"total_received":\K[0-9]+' | awk '{s=$1/100000000} END {printf "%0.8f\n", s}')
-#echo "Balance : $balance"
+    echo $ADDRESS;
+    wget -qO- https://blockchain.info/balance?active=$ADDRESS > ./tbalance
+    #balance=$(wget -qO- https://blockchain.info/balance?active=$ADDRESS 2>&1 | grep -Po '"total_received":\K[0-9]+' | awk '{s=$1/100000000} END {printf "%0.8f\n", s}')
 
-transactions=$(cat ./tbalance | grep -Po '"n_tx":\K[0-9]+' | awk '{s=$1/1} END {printf "%0.1f\n", s}')
-balance=$(cat ./tbalance | grep -Po '"total_received":\K[0-9]+' | awk '{s=$1/100000000} END {printf "%0.8f\n", s}')
-echo "Balance : $balance"
-echo "transactions : $transactions"
+    transactions=$(cat ./tbalance | grep -Po '"n_tx":\K[0-9]+' | awk '{s=$1/1} END {printf "%0.1f\n", s}')
+    balance=$(cat ./tbalance | grep -Po '"total_received":\K[0-9]+' | awk '{s=$1/100000000} END {printf "%0.8f\n", s}')
+    echo "Balance : $balance"
+    echo "Tx number : $transactions"
 
-if [[ $balance != $oldbalance ]]; then
-    #beep
-    echo -en "\007"
-    spd-say 'Funds Have Been Deposited'
-    echo "balance updated"
-fi
-oldbalance=$balance
-#sleep 10
-sleep 1500 #10-15 min
+    if [[ $balance != $oldbalance ]]; then
+        #beep
+        echo -en "\007"
+        spd-say 'Funds Have Been Deposited'
+        echo "balance updated"
+    fi
+    
+    oldbalance=$balance
+    
+    #sleep 100 #testing offline only
+    sleep 1500 #10-15 min
 done
-#0.05185304
 
