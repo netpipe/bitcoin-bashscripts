a1b21a148c60e266cc814dfcd2e8d6263b88ae28
netpipe <teslaenergy@gmail.com>
2022-11-30 00:06:35 -0700

    testing

--- cb.sh
+++ cb.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 #useful for waiting for payments or seeing your wallet balance live
-ADDRESS="bc1qu3ds562w2av52rz8xl0pm5jkk66s5ep95e4wwp" #using my own for now you can replace with yours and use ./cb.sh 1 to use it or type actual address
+ADDRESS2="bc1qu3ds562w2av52rz8xl0pm5jkk66s5ep95e4wwp" #using my own for now you can replace with yours and use ./cb.sh 1 to use it or type actual address
 oldbalance=0
 n=0
 
-if [[ $1 > "" ]] ; then ADDRESS=$1 ; fi # use alternate address if specified
-if [[ $1 = 1 ]] ; then ADDRESS=$ADDRESS ; fi # use alternate address if specified
+if [[ $1 > "" ]] ; then ADDRESS="" ; fi # use alternate address if specified
+if [[ $1 = "1" ]] ; then ADDRESS=$ADDRESS2 ; fi # use alternate address if specified
 if [[ $1 = "" ]] ; then echo "insert address" ; fi # use alternate address if specified
 while true;do
     n=$(($n+1))
     echo $ADDRESS;
-    wget -qO- https://blockchain.info/balance?active=$ADDRESS > ./tbalance
+    if [[ $ADDRESS > "" ]] ; then
+        wget -qO- https://blockchain.info/balance?active=$ADDRESS > ./tbalance
+    fi
     #balance=$(wget -qO- https://blockchain.info/balance?active=$ADDRESS 2>&1 | grep -Po '"total_received":\K[0-9]+' | awk '{s=$1/100000000} END {printf "%0.8f\n", s}')
 
     transactions=$(cat ./tbalance | grep -Po '"n_tx":\K[0-9]+' | awk '{s=$1/1} END {printf "%0.1f\n", s}')
