4f87e4b03e5a04259b853f04a97c75146548c780
netpipe <teslaenergy@gmail.com>
2022-11-30 22:36:18 -0700

    testing

--- cb.sh
+++ cb.sh
@@ -9,15 +9,19 @@ if [[ $1 = "1" ]] ; then ADDRESS=$ADDRESS2 ; fi # use alternate address if speci
 if [[ $1 = "" ]] ; then echo "insert address" ; fi # use alternate address if specified
 while true;do
     n=$(($n+1))
-    echo $ADDRESS;
-    if [[ $ADDRESS > "" ]] ; then
-        wget -qO- https://blockchain.info/balance?active=$ADDRESS > ./tbalance
+    #echo $ADDRESS;
+    if [[ $ADDRESS != "" ]] ; then
+    echo "$ADDRESS"
+     #   wget -qO- https://blockchain.info/balance?active=$ADDRESS > ./tbalance
     fi
     #balance=$(wget -qO- https://blockchain.info/balance?active=$ADDRESS 2>&1 | grep -Po '"total_received":\K[0-9]+' | awk '{s=$1/100000000} END {printf "%0.8f\n", s}')
 
     transactions=$(cat ./tbalance | grep -Po '"n_tx":\K[0-9]+' | awk '{s=$1/1} END {printf "%0.1f\n", s}')
     balance=$(cat ./tbalance | grep -Po '"total_received":\K[0-9]+' | awk '{s=$1/100000000} END {printf "%0.8f\n", s}')
-    echo "Balance : $balance"
+    
+  fbalance=$(cat ./tbalance | grep -Po '"final_balance":\K[0-9]+' | awk '{s=$1/100000000} END {printf "%0.8f\n", s}')
+    echo "Final Balance : $fbalance"
+    echo "Received : $balance"
     echo "Tx number : $transactions"
     echo "wget count : $n"
     if [[ $balance != $oldbalance ]]; then
